801476c443dc3f32f6489b31dc3faecd08dfa4ac
VENIO LUIZ DE PAULA FERREIRA <venioferreira@yahoo.com.br>
2019-10-11 08:49:27 -0300

    atualizacao projeto

--- terraform/infra/notifications/sqs_sms.tf
+++ terraform/infra/notifications/sqs_sms.tf
@@ -1,6 +1,11 @@
 resource "aws_sqs_queue" "sms" {
   name = "${var.environment}-sms-queue"
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.sms_dlq.arn}\",\"maxReceiveCount\":3}"
+  redrive_policy = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.sms_dlq.arn}\",\"maxReceiveCount\":3}"
+  policy = templatefile("${path.module}/templates/sqs-sns-policy.tpl",
+    {
+        resource = "arn:aws:sqs:${var.region}:${var.account_id}: ${var.environment}-sms-queue"
+        source_arn = "${aws_sns_topic.notifications.arn}"
+    })
 }
 
 resource "aws_sqs_queue" "sms_dlq" {
